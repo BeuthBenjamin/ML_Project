2a73d09ff2777a871931714f2aead91c5b1034d9
tsteffek <thomas.steffek@beuth-hochschule.de>
2021-01-10 23:00:56 +0100

    fix(HPO): works now

--- SVM/HPO.R
+++ SVM/HPO.R
@@ -1,41 +1,73 @@
 source('SVM/SVM.R')
 
 # all options
-scales <- c(TRUE, FALSE)
-costs <- 10^(1:3)
-gammas <- 10^(-3:-1)
-degrees <- 2:4
+costs <- 10^(0:3)
+gammas <- c(0, 1/18, 10^(-3:-1)) # 1/18 is the gamma default value, 1/dimensions of the data
+degrees <- 2:5
+coef0s <- c(0, 10^-3:3)
 
-log <- function(tuned, kernel_name){
-  cat("\n##############\n\nBest model for ", kernel_name, ":\n\n##############\n\nParameters:\n")
+add_kernel <- function(obj, kernel_name){
+  obj['kernel'] <- kernel_name
+  return(obj)
+}
+
+log <- function(tuned){
+  cat("\n##############\n\nBest model for ", tuned$kernel, ":\n\n##############\n\nParameters:\n")
   print(tuned$best.parameters)
   print(confusionMatrix(predict(tuned$best.model, x_val), y_val))
   return(tuned)
 }
-
-lin_tuned <- log(tune.svm(ContraceptiveMethodUsed~., data = train,
-                          scale = scales,
+# Tune each kernel
+lin_tuned <- log(add_kernel(tune.svm(ContraceptiveMethodUsed~., data = train,
                           cost = costs,
                           kernel = "linear",
                           validation.x = x_val, validation.y = y_val,
                           tunecontrol = tune.control(sampling = "fix")
-), "linear")
+), "linear"))
 
-rad_tuned <- log(tune.svm(ContraceptiveMethodUsed~., data = train,
-                          scale = scales,
+rad_tuned <- log(add_kernel(tune.svm(ContraceptiveMethodUsed~., data = train,
                           cost = costs,
                           gamma = gammas,
                           kernel = "radial",
                           validation.x = x_val, validation.y = y_val,
                           tunecontrol = tune.control(sampling = "fix")
-), "radial")
+), "radial"))
 
-poly_tuned <- log(tune.svm(ContraceptiveMethodUsed~., data = train,
-                           scale = scales,
+poly_tuned <- log(add_kernel(tune.svm(ContraceptiveMethodUsed~., data = train,
                            cost = costs,
                            gamma = gammas,
                            degree = degrees,
+                           coef0 = coef0s,
                            kernel = "polynomial",
                            validation.x = x_val, validation.y = y_val,
                            tunecontrol = tune.control(sampling = "fix")
-), "polynomial")
+), "polynomial"))
+
+sig_tuned <- log(add_kernel(tune.svm(ContraceptiveMethodUsed~., data = train,
+                           cost = costs,
+                           gamma = gammas,
+                           coef0 = coef0s,
+                           kernel = "sigmoid",
+                           validation.x = x_val, validation.y = y_val,
+                           tunecontrol = tune.control(sampling = "fix")
+), "sigmoid"))
+
+# Find the best model
+all_models <- list(lin_tuned, rad_tuned, poly_tuned, sig_tuned)
+Best.performances <- lapply(all_models, function(x) x[["best.performance"]])
+best_model <- all_models[which.min(Best.performances)][[1]]
+
+# train our final model with both validation and training data
+final_svm_model <- do.call(svm, c(
+  formula = ContraceptiveMethodUsed ~ .,
+  data = list(train_and_validation),
+  kernel = best_model$kernel,
+  best_model$best.parameters
+)
+)
+
+# Apply the model to the test set
+confusionMatrix(
+  predict(final_svm_model, subset(test, select = -ContraceptiveMethodUsed)),
+  test$ContraceptiveMethodUsed
+)
--- SVM/SVM.R
+++ SVM/SVM.R
@@ -4,28 +4,14 @@ library(caret)
 # import data
 source("Data/import.R")
 
-# train, validation, test split as required
-set.seed(2021)
-idx <-
-  sample(
-    seq(1, 3),
-    size = nrow(data),
-    replace = TRUE,
-    prob = c(0.6, 0.2, 0.2)
-  )
-train <- data[idx == 1,]
-validation <- data[idx == 2,]
-test <- data[idx == 3,]
-
 # train a default SVM
 svm_model <-
   svm(
-    ContraceptiveMethodUsed ~ .,
+    formula = ContraceptiveMethodUsed ~ .,
     data = train,
-    scale = TRUE,
     kernel = "polynomial",
-    cost = 10,
-    degree = 3,
+    cost = 1,
+    degree = 3
   )
 
 summary(svm_model)
